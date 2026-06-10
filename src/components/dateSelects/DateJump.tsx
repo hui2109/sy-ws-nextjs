3cbac0edacdcafa0c35c663f2287feacd3875351
kukudehui <995631664@qq.com>
2026-06-10 22:43:20 +0800

    1. 优化日期跳转组件

--- src/components/dateSelects/DateJump.tsx
+++ src/components/dateSelects/DateJump.tsx
@@ -9,18 +9,35 @@ type Picker = 'month' | 'year';
 export default function DateJump({picker}: { picker: Picker }) {
     const format = picker === 'month' ? 'YYYY年M月' : 'YYYY年';
     const [current, setCurrent] = useState<dayjs.Dayjs>(dayjs());
+    const [open, setOpen] = useState<boolean>(false);  // 控制弹出层
 
     function onChange(date: dayjs.Dayjs | null): void {
         if (date) setCurrent(date);
     }
 
     function handlePreClick() {
-        setCurrent(prev => prev.subtract(1, picker));  // picker 直接是 'month' 或 'year'
+        setCurrent(prev => prev.subtract(1, picker));
     }
 
     function handleNexClick() {
         setCurrent(prev => prev.add(1, picker));
     }
+
+    function handleJumpToToday() {
+        setCurrent(dayjs());
+        setTimeout(() => setOpen(false), 100);
+    }
+
+    const extraFooter = (
+        <div className="flex justify-between">
+            <Button type="default" onClick={handleJumpToToday} size="small">
+                选择今天
+            </Button>
+            <Button type="default" onClick={() => setOpen(false)} size="small">
+                关闭
+            </Button>
+        </div>
+    );
 
     return (
         <Space>
@@ -28,12 +45,18 @@ export default function DateJump({picker}: { picker: Picker }) {
                 {picker === 'month' ? '上个月' : '上一年'}
             </Button>
             <DatePicker
-                value={current}           // 受控模式
+                value={current}                      // 受控日期模式
+                open={open}                          // 受控弹出层
+                // onOpenChange={setOpen}               // 点击输入框、外部点击时同步
+                onOpenChange={() => {
+                    setOpen(!open)
+                }}               // 点击输入框、外部点击时同步
                 onChange={onChange}
                 picker={picker}
                 format={format}
                 inputReadOnly={true}
                 allowClear={false}
+                renderExtraFooter={() => extraFooter}
             />
             <Button type="primary" onClick={handleNexClick}>
                 {picker === 'month' ? '下个月' : '下一年'}
